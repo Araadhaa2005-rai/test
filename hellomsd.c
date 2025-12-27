044d0c23720c6311a92f920eab84c902c629500e
Araadhaa2005-rai <dr.anro2005@gmail.com>
2025-12-27 19:30:17 +0530

    change

---
+++ hellomsd.c
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+#include<math.h>
+int main()
+{
+    int a=10;
+    int b=12;
+    int c=16;
+    printf("%d",& (a+b+c)/3);
+    return 0;
+}
---
+++ practice session.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    int b;
+   scanf("%d",&a);
+   scanf("%d",&b);
+   printf("\n %d", a+b);
+   printf("\n %d",a-b);
+   printf("\n %d",a*b);
+   printf("\n %d", a/b);
+   return 0;
+
+}
