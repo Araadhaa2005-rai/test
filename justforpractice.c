d4df82e0b064338b1415fd108fc707d4952c9250
Araadhaa2005-rai <dr.anro2005@gmail.com>
2025-12-30 18:37:53 +0530

    change

---
+++ justforpractice.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int sum1 = 5*2/4+2/7-5;
+    int sum2 = 4-10*56*12+32;
+    int sum3 = 16/12+34-21;
+    
+    printf("%d\n",sum1);
+    printf("%d\n",sum2);
+     printf("%d\n",sum3);
+    
+    
+
+
+}
